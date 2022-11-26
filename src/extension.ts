d14bacaa75e3c8a25092c3a77d8a09206a084b23
Mikhail Shcherbakov <118803353+whafrothemoo@users.noreply.github.com>
2022-11-26 12:02:54 +0300

    Implement "Eval selection or line"

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,18 @@ import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
 
+let sclang : child_process.ChildProcessWithoutNullStreams | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('scvscode.start', () => {
 		start();
+	});
+
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('scvscode.evalSelectionOrLne', () => {
+		evalSelectionOrLine();
 	});
 
 	context.subscriptions.push(disposable);
@@ -22,9 +30,35 @@ function start() {
 
 	// Not sure that scvscode is proper IDE as sclang -i
 	// TODO: reconsider IDE name
-	const sclang = child_process.spawn('sclang', ['-i', 'scvscode']);
+	sclang = child_process.spawn('sclang', ['-i', 'scvscode']);
 
 	sclang.stdout.on('data', data => {
 		sclangOutputChannel.appendLine(data.toString());
 	});	
+}
+
+function evalSelectionOrLine() {
+
+	const editor = vscode.window.activeTextEditor;
+	let textToEval;
+
+	if (editor)
+	{
+		if (editor.selection.isEmpty) {
+			textToEval =
+				editor.document.lineAt(editor.selection.active.line).text;
+		} else {
+			textToEval = editor.document.getText(editor.selection);
+		}
+	}
+
+	textToEval += String.fromCharCode(0x1b, 0x0d);
+
+	if (sclang === undefined) {
+		start();
+	}
+
+	if (sclang !== undefined) {
+		sclang.stdin.write(textToEval);
+	}
 }
